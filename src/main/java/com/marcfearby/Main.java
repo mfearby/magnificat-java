62ce266419db97e4e4cae48e839cbb03be071131
Marc Fearby <mrf.sites@icloud.com>
2018-05-26 13:39:49 +1000

    Added support for Maven

--- src/main/java/com/marcfearby/Main.java
+++ src/main/java/com/marcfearby/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/window.fxml"));
         primaryStage.setTitle("Magnificat");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
