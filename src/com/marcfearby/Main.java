ab56edeaf661f979c4b13f8c45ed8b305730c039
Marc Fearby <mrf.sites@icloud.com>
2018-05-26 12:34:06 +1000

    Initial JavaFX sample project

---
+++ src/com/marcfearby/Main.java
@@ -0,0 +1,23 @@
+package com.marcfearby;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
+        primaryStage.setTitle("Magnificat");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
