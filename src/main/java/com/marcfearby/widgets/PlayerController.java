bd791400afdc2b5144002d172a82f05c066286d8
Marc Fearby <mrf.sites@icloud.com>
2018-06-17 22:13:48 +1000

    Started working on the music player UI

---
+++ src/main/java/com/marcfearby/widgets/PlayerController.java
@@ -0,0 +1,13 @@
+package com.marcfearby.widgets;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+
+public class PlayerController {
+
+    @FXML public HBox player;
+
+
+    public Button playButton;
+}
