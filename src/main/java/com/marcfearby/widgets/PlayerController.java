4b340aa2bc699e35792c2c74c635ddff2aafdecc
Marc Fearby <mrf.sites@icloud.com>
2018-06-18 21:08:21 +1000

    Finished adding UI components to the PlayerView

--- src/main/java/com/marcfearby/widgets/PlayerController.java
+++ src/main/java/com/marcfearby/widgets/PlayerController.java
@@ -1,13 +1,28 @@
 package com.marcfearby.widgets;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PlayerController {
+public class PlayerController implements Initializable {
 
     @FXML public HBox player;
+    @FXML public Button playButton;
+    @FXML public Button pauseButton;
+    @FXML public Button backButton;
+    @FXML public Button stopButton;
+    @FXML public Button forwardButton;
+    @FXML public Label progressLabel;
+    @FXML public Label remainingLabel;
+    @FXML public Button muteButton;
+    @FXML public Button unmuteButton;
 
-
-    public Button playButton;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
 }
